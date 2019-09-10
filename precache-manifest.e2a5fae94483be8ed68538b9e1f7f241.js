ead5560660b03456418f5bcd5f1a76797860c325
Himanshu Rana <himanshu.rana2111@gmail.com>
2019-09-10 15:05:24 +0530

    Updates

---
+++ precache-manifest.e2a5fae94483be8ed68538b9e1f7f241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccc1bba8d6dc73528009d249fb5581d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9040bd82d5243ad040f2",
+    "url": "/portfolio/static/css/main.ae9edefb.chunk.css"
+  },
+  {
+    "revision": "0988c72a55cea92f67d3",
+    "url": "/portfolio/static/js/2.a918f20b.chunk.js"
+  },
+  {
+    "revision": "9040bd82d5243ad040f2",
+    "url": "/portfolio/static/js/main.8b06013e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
